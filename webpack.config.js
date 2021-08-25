857b65b65879136b4fba1eea56ebc2b9db261022
Philip Soriano <philip.soriano@hotmail.com>
2021-08-25 15:54:46 +0400

    fixed config

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,64 @@
-const path = require('path');
+const path = require('path')
 const HTMLWebPackPlugin = require('html-webpack-plugin')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 
+
 module.exports = {
-  entry:{
-    main:'./src/main.js',
-    home: './src/components/home.js',
-    about: './src/components/about.js',
-    menu: './src/components/menu.js',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module:{
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use:[
-          { 
-            loader: "babel-loader"
-          }
+    mode:'development',
+    entry: {
+        './main':'./src/main.js',
+        './components/about':'./src/components/about.js',
+        './components/home':'./src/components/home.js',
+        './components/menu':'./src/components/menu.js',
+        './styles/styles':'./src/styles/styles.scss',
+    },
+    plugins: [
+        new MiniCSSExtractPlugin(),
+        new HTMLWebPackPlugin({
+            template:path.resolve(__dirname, 'src') + "/index.html",
+            filename: "./index.html"
+          }),
+    ],
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.(s[ac]ss|css)$/i,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    "css-loader", 
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader:"file-loader",
+                options: {
+                    name: './assets/images/[name].[ext]',
+                  },
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: './assets/fonts/[name].[ext]',
+                    }
+                  }
+                ]
+            },
         ]
-      },
-      {
-        test: /\.html$/i,
-        use: [
-          {
-            loader: "html-loader",
-            options: {minimize: true}
-          },
-        ]
-      },
-      {
-        test: /\.(png|svg|gif|jpg|webp)$/,
-        use:[
-          {
-            loader: "file-loader"
-          }
-        ]
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {   test: /\.scss$/,   use: ["style-loader", "css-loader", "sass-loader"] }
-    ]
-  },
-  plugins: [
-    new HTMLWebPackPlugin({
-      template:path.resolve(__dirname, 'src') + "/index.html",
-      filename: "./index.html"
-    }),
-    new MiniCSSExtractPlugin({
-      //options similar to the options in webpackoptions.output
-      //both options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ],
-  output: {
-    filename: 'widget.[contenthash].js',
-    // path: path.resolve(bundleOutputDir),
-  },
-  externals: {
-    aos: 'aos',
-  },
-  
-};
+    },
+    externals: {
+      aos: 'aos',
+    },
+}
+ 
 
 
